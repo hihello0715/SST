30ddde86a6b1c2df95e02eaea2e171093211c62b
hihello <aa0715@naver.com>
2020-11-26 15:29:08 +0900

    테스트

--- app/src/main/java/com/example/sst2/MainActivity.java
+++ app/src/main/java/com/example/sst2/MainActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv_id, tv_pass;
+    private TextView tv_id, tv_name, btn_room, btn_matching;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +16,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv_id =findViewById(R.id.tv_id);
-        tv_pass= findViewById(R.id.tv_pass);
+        tv_name =findViewById(R.id.tv_name);
+        btn_room = findViewById(R.id.btn_room);
+        btn_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RoomActivity.class);
+                startActivity(intent); //액티비티 이동
+            }
+        });
+        btn_matching = findViewById(R.id.btn_matching);
+        btn_matching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MatchingActivity.class);
+                startActivity(intent); //액티비티 이동
+            }
+        });
+
         Intent intent = getIntent();
         String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
+        String userName = intent.getStringExtra("userName");
+
+
 
         tv_id.setText(userID);
-        tv_pass.setText(userPass);
+        tv_name.setText(userName);
+
+
     }
 }
